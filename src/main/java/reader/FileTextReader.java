73ba986a2bb4694f96312ebef768eba2040a7447
Natalie <natalli.buyanova.01@mail.ru>
2020-02-22 20:18:53 +0300

    Add logger to FileTextReader

--- src/main/java/reader/FileTextReader.java
+++ src/main/java/reader/FileTextReader.java
@@ -1,6 +1,8 @@
 package reader;
 
 import exception.ReaderException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,11 +11,13 @@ import java.nio.file.Files;
 
 public class FileTextReader {
 
+    private static Logger logger = LogManager.getLogger();
     public String getText(File file) throws ReaderException {
         String text;
         try {
             text = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
         } catch (IOException e) {
+            logger.warn(e);
             throw new ReaderException(e);
         }
         return text;
